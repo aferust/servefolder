006bdefd484f37d96e86ecafb45006acb7d9a0d0
aferust <aferust@gmail.com>
2024-03-01 15:14:00 +0300

    update readme for getopt

--- source/app.d
+++ source/app.d
@@ -7,6 +7,9 @@ import std.process : environment;
 import std.json : JSONValue, parseJSON, toJSON;
 import std.string;
 import std.experimental.logger;
+import std.getopt;
+
+import core.stdc.stdlib : exit;
 
 import core.runtime: Runtime;
 
@@ -22,10 +25,25 @@ ushort port = 8080;
 @onServerInit ServerinoConfig setup(string[] args)
 {
     // we can only handle port here, the rest will be handled in onDaemonStart
+    auto evar = environment.get("servefolder_paths", null);
 
-    if(args.length > 2){
-        port = args[2].to!ushort;
+    if(evar is null){
+
+        string unused;
+
+        auto helpInformation = getopt( args, std.getopt.config.bundling,
+            "directory|d", &unused,
+            "port|p", &port
+        );
+
+        if (helpInformation.helpWanted)
+        {
+            defaultGetoptPrinter("help information.",
+            helpInformation.options);
+            exit(0);
+        }
     }
+    
 
     ServerinoConfig sc = ServerinoConfig.create(); // Config with default params
     sc.addListener("0.0.0.0", port);
@@ -36,21 +54,25 @@ ushort port = 8080;
 
 @onDaemonStart void daemonStart(){
 
-    const args = Runtime.args();
+    auto args = Runtime.args();
 
     auto evar = environment.get("servefolder_paths", null);
 
     if(evar is null){
-        if(args.length > 1)
-            folder = args[1];
-        else
-            folder = dirName(thisExePath());
-        
+        folder = dirName(thisExePath());
+
+        auto helpInformation = getopt( args, std.getopt.config.bundling,
+            "directory|d", &folder,
+            "port|p", &port
+        );
+
         _files = listFilesRecursively(folder);
         
         JSONValue payload;
         payload.array = _files;
         environment["servefolder_paths"] = toJSON(payload);
+
+        log("The server has started to serve from the folder : ", folder);
     }
 }
 
